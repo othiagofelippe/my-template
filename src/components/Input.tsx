6995fe6aa38bc5aaeb16cb75eb257e22c1b93ef5
othiagofelippe <thiagofelippe135@gmail.com>
2024-03-17 16:46:33 -0300

    feat(components): add Input component for form fields

---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import React, { ComponentProps } from "react";
+
+interface InputProps extends ComponentProps<"input"> {
+  label: string;
+}
+
+export function Input({ label, ...rest }: InputProps) {
+  return (
+    <div>
+      <label
+        htmlFor={rest.id}
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          {...rest}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+        />
+      </div>
+    </div>
+  );
+}
